2729f772a95de1c80c89dd19ce69f1d91b6cbed8
yunalee2490 <yunalee2490@gmail.com>
2025-11-11 10:17:43 +0900

    3장 commit

--- springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/TestController.java
+++ springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/TestController.java
@@ -1,16 +1,18 @@
 package me.shinsunyoung.springbootdeveloper;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@Controller
-@ResponseBody
-class TestConroller {
+public class TestController {
+    @Autowired
+    TestService testService;
     @GetMapping("/test")
-    public String test(){
-        return "Hello, World!";
+    public List<Member> getAllMembers() {
+        List<Member> members = testService.getAllMembers();
+        return members;
     }
 }
